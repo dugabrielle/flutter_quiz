d1c976dc472803a05a989651985334dc4ffbf390
Gabi <dugabrielle9@gmail.com>
2025-06-27 15:37:45 -0300

    alteração da cor do Container

--- lib/src/ui/pages/boas_vindas.dart
+++ lib/src/ui/pages/boas_vindas.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shimmer/shimmer.dart';
-import 'package:flutter_quizz/src/data/medias/screens_width.dart';
+import 'package:flutter_quizz/src/ui/medias/screens_width.dart';
 
 class BoasVindas extends StatefulWidget {
   const BoasVindas({super.key});
@@ -16,98 +16,106 @@ class _BoasVindasState extends State<BoasVindas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 215, 230, 240),
+      backgroundColor: Colors.transparent,
       body: SafeArea(
-        child: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 100,
-                  horizontal: 60,
-                ),
-                decoration: BoxDecoration(
-                  color: Color(0x66FFFFFF),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Image.asset(
-                      'assets/logo_app.png',
-                      width:
-                          (ScreensWidth.isDesktop(context) ||
-                                  ScreensWidth.isLargeDesktop(context))
-                              ? MediaQuery.sizeOf(context).width * 0.25
-                              : (ScreensWidth.isTablet(context))
-                              ? MediaQuery.sizeOf(context).width * 0.4
-                              : MediaQuery.sizeOf(context).width * 0.5,
-                      fit: BoxFit.cover,
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 13, 71, 161),
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(70),
+                        bottomRight: Radius.circular(70),
+                      ),
                     ),
-                    Shimmer.fromColors(
-                      baseColor: Colors.red[900]!,
-                      highlightColor: Colors.yellow[700]!,
-                      child: Text(
-                        'Vamos Jogar!',
-                        style: GoogleFonts.cabinSketch(
-                          textStyle: const TextStyle(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(50),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        'assets/images/logo_app.png',
+                        width:
+                            (ScreensWidth.isDesktop(context) ||
+                                    ScreensWidth.isLargeDesktop(context))
+                                ? MediaQuery.sizeOf(context).width * 0.25
+                                : (ScreensWidth.isTablet(context))
+                                ? MediaQuery.sizeOf(context).width * 0.4
+                                : MediaQuery.sizeOf(context).width * 0.5,
+                        fit: BoxFit.cover,
+                      ),
+                      Shimmer.fromColors(
+                        baseColor: const Color.fromARGB(255, 122, 13, 13),
+                        highlightColor: Colors.yellow[700]!,
+                        child: Text(
+                          'Vamos Jogar!',
+                          style: GoogleFonts.cabinSketch(
+                            textStyle: const TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 30),
-                    MouseRegion(
-                      onEnter: (event) {
-                        setState(() {
-                          isHover = true;
-                        });
-                      },
-                      onExit: (event) {
-                        setState(() {
-                          isHover = false;
-                        });
-                      },
-                      child: Transform.scale(
-                        scale: isHover ? 1.10 : 1.0,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor: WidgetStateProperty.all<Color>(
-                              Colors.black,
-                            ),
-                            padding: WidgetStateProperty.all<EdgeInsets>(
-                              EdgeInsets.symmetric(
-                                horizontal: 40,
-                                vertical: 15,
+                      const SizedBox(height: 30),
+                      MouseRegion(
+                        onEnter: (event) {
+                          setState(() {
+                            isHover = true;
+                          });
+                        },
+                        onExit: (event) {
+                          setState(() {
+                            isHover = false;
+                          });
+                        },
+                        child: Transform.scale(
+                          scale: isHover ? 1.10 : 1.0,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStateProperty.all<Color>(
+                                Colors.black,
+                              ),
+                              padding: WidgetStateProperty.all<EdgeInsets>(
+                                EdgeInsets.symmetric(
+                                  horizontal: 40,
+                                  vertical: 15,
+                                ),
+                              ),
+                              shape: WidgetStateProperty.all<
+                                RoundedRectangleBorder
+                              >(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                ),
                               ),
                             ),
-                            shape:
-                                WidgetStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                  ),
-                                ),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            'Iniciar',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
+                            onPressed: () {},
+                            child: const Text(
+                              'Iniciar',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
