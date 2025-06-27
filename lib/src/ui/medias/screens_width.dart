ed8a29fd4cd22f2b414f6f3d1baa126590ba2de1
Gabi <dugabrielle9@gmail.com>
2025-06-27 15:38:43 -0300

    classe ScreensWidth para diferentes tamanhos de tela

---
+++ lib/src/ui/medias/screens_width.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ScreensWidth {
+  static bool isMobile(BuildContext context) =>
+      MediaQuery.of(context).size.width < 600;
+
+  static bool isTablet(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 600 &&
+      MediaQuery.of(context).size.width < 1024;
+
+  static bool isDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1024;
+
+  static bool isLargeDesktop(BuildContext context) =>
+      MediaQuery.of(context).size.width >= 1440;
+}
