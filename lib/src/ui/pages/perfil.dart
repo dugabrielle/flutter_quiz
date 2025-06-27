05d03ed67211704385440850a316652a5c412553
Gabi <dugabrielle9@gmail.com>
2025-06-27 14:39:28 -0300

    construção da tela de perfil do usuário

---
+++ lib/src/ui/pages/perfil.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+
+class PerfildoUsuario extends StatefulWidget {
+  const PerfildoUsuario({super.key});
+
+  @override
+  State<PerfildoUsuario> createState() => _PerfildoUsuarioState();
+}
+
+class _PerfildoUsuarioState extends State<PerfildoUsuario> {
+  final TextEditingController _nomeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PerfilAppBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.0, 0.2, 0.5, 1.0],
+                  colors: [
+                    Colors.black,
+                    Colors.black,
+                    Colors.red,
+                    Colors.yellow,
+                  ],
+                ),
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Perfil'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  CircleAvatar(radius: 80, backgroundColor: Colors.grey),
+                  CircleAvatar(
+                    radius: 75,
+                    backgroundImage: const AssetImage('images/german.png'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+
+              SizedBox(
+                width: 250,
+                child: TextField(
+                  controller: _nomeController,
+                  maxLines: 1,
+                  maxLength: 20,
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 120,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.yellow),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                      final nome = _nomeController.text;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Nome salvo: $nome')),
+                      );
+                    },
+                    child: const Text(
+                      'Salvar',
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PerfilAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const PerfilAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: preferredSize.height,
+      decoration: const BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.black, width: 2)),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+      ),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.black),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
+              const Text(
+                'Meu Perfil',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80);
+}
